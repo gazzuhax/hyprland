93bec94b5bf3b5d45650bbef65d1609249ee71fa
CyberTux <138269636+gazzuhax@users.noreply.github.com>
2023-10-22 22:59:52 +0800

    Update LockScreen.sh

--- .config/hypr/scripts/LockScreen.sh
+++ .config/hypr/scripts/LockScreen.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-
-CONFIG="$HOME/.config/hypr/swaylock/config"
-
-sleep 0.5s; swaylock --config ${CONFIG} & disown
+swaylock  \
+       --screenshots \
+       --clock \
+       --timestr %T \
+       --datestr %a, %x \
+       --indicator \
+       --indicator-radius 200 \
+       --indicator-thickness 7 \
+       --effect-blur 7x5 \
+       --effect-vignette 0.5:0.1 \
+       --ring-color 3b4252 \
+       --key-hl-color 880033 \
+       --line-color 00000000 \
+       --inside-color 00000088 \
+       --separator-color 00000000 \
+       --grace 2 \
+       --fade-in 0.1
