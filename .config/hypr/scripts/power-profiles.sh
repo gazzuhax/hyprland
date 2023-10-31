1a9307d7fe39aaf7156969e0a0e4ba307c8d1e32
CyberTux <138269636+gazzuhax@users.noreply.github.com>
2023-11-01 04:58:48 +0800

    Update power-profiles.sh

--- .config/hypr/scripts/power-profiles.sh
+++ .config/hypr/scripts/power-profiles.sh
@@ -2,46 +2,48 @@
 set -x
 #
 # power-profiles-daemon implementation:
-# needs package power-profiles-daemon installed and the service running see here:
+# needs package power-profiles-daemon installed and the service running. See here:
 # https://wiki.archlinux.org/title/CPU_frequency_scaling#power-profiles-daemon
-# used in i3-blocks: ~/.config/i3/i3blocks.conf together with: ~/.config/i3/scripts/power-profiles
 
 # Function to set the power profile
 set_power_profile() {
   /usr/bin/powerprofilesctl set "$1" > /dev/null
+  notify-send "Power Profile Set" "Power profile set to $1."
+}
+
+# Function to get the current power profile
+get_power_profile() {
+  current_profile=$(/usr/bin/powerprofilesctl get)
+  notify-send "Current Power Profile" "Current power profile: $current_profile"
 }
 
 # Menu with keys/commands
 menu=(
-  [1]="Performance"
-  [2]="Balanced"
-  [3]="Power Saver"
-  [4]="Cancel"
+  [1]=" Performance"
+  [2]=" Balanced"
+  [3]=" Power Saver"
+  [4]=" Get Power Profile"
+  [5]=" Cancel"
 )
 
 # Prompt for user choice using rofi with the custom theme file
 choice=$(printf '%s\n' "${menu[@]}" | rofi -dmenu -i -p "Select power profile:" \
   -theme ~/.config/rofi/power-profiles.rasi)
 
-# Check user's choice and set power profile accordingly
+# Check user's choice and take appropriate action
 case "$choice" in
-  "Performance") set_power_profile "performance"
-                 echo "Power profile set to performance."
+  " Performance") set_power_profile "performance"
                  ;;
-  "Balanced") set_power_profile "balanced"
-              echo "Power profile set to balanced."
+  " Balanced") set_power_profile "balanced"
               ;;
-  "Power Saver") set_power_profile "power-saver"
-                 echo "Power profile set to power-saver."
+  " Power Saver") set_power_profile "power-saver"
                  ;;
-  "Cancel") echo "Operation canceled."
+  " Get Power Profile") get_power_profile
+                       ;;
+  " Cancel") notify-send "Operation Canceled" "Operation canceled."
             exit 0
             ;;
-  *) echo "Invalid choice. Exiting."
+  *) notify-send "Invalid Choice" "Invalid choice. Exiting."
      exit 1
      ;;
 esac
-
-# Get the current power profile
-current_profile=$(/usr/bin/powerprofilesctl get)
-echo "Current power profile: $current_profile"
